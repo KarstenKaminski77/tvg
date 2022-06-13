82ead4f8ea32c7756ff7cc7c83e9feb7ef27c8e1
Karsten Kaminski <karstenkaminski77@gmail.com>
2022-06-13 10:32:54 +0200

    debugging

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -3164,7 +3164,7 @@ class OrdersController extends AbstractController
         }
  
         $snappy = new Pdf(__DIR__ .'/../../vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64');
-\
+
         $html = '
         <table style="width: 100%; border: none; border-collapse: collapse; font-size: 12px">
             <tr>
