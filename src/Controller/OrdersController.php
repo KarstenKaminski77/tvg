e0a77eea359d3071877ced1a52f1dffb80e372c7
Karsten Kaminski <karstenkaminski77@gmail.com>
2022-06-17 14:05:07 +0200

    mobile filter panel

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -2234,40 +2234,49 @@ class OrdersController extends AbstractController
                     </button>
                 </div>
                 
-                <div class="col-12 col-sm-5 d-flex  pt-3 d-block d-sm-none">
-                    '. $distributors_select .'
-                </div>
-                <div class="col-12 col-sm-5 d-flex pt-3 d-block d-sm-none">
-                    <input 
-                        type="text" 
-                        class="form-control datepicker" 
-                        name="datetimes" 
-                        id="datepicker_mobile" 
-                        autocomplete="off"
-                        value="Date"
-                        placeholder="Date"
-                    >
-                </div>
-                <div class="col-12 col-sm-5 d-flex pt-3 d-block d-sm-none">
-                    '. $status_select .'
-                </div>
-                <div class="col-12 pt-3 d-block d-sm-none">
-                    <button 
-                        class="btn btn-primary distributor_search w-sm-100 text-center"
-                        data-clinic-id="'. $clinic->getId() .'"
-                    >
-                        <i class="fa-solid fa-magnifying-glass me-3"></i>
-                        SEARCH
-                    </button>
-                </div>
-                <div class="col-12 pt-3 pb-3 d-block d-sm-none">
-                    <button 
-                        class="btn btn-secondary distributor_refresh w-sm-100 text-center"
-                        data-clinic-id="'. $clinic->getId() .'"
-                    >
-                        <i class="fa-solid fa-rotate me-3"></i>
-                        CANCEL
-                    </button>
+                <div class="col-12 d-block d-sm-none">
+                    <div class="row border-bottom">
+                        <div role="button" class="col-12 text-danger pt-3 pb-3" id="filter_orders">
+                            <i class="fa-solid fa-filter me-3"></i>Filter Orders
+                        </div>
+                    </div>
+                    <div class="row hidden" id="filter_row">
+                        <div class="col-12 col-sm-5 d-flex pt-3 d-block d-sm-none">
+                            '. $distributors_select .'
+                        </div>
+                        <div class="col-12 col-sm-5 d-flex pt-3 d-block d-sm-none">
+                            <input 
+                                type="text" 
+                                class="form-control datepicker" 
+                                name="datetimes" 
+                                id="datepicker_mobile" 
+                                autocomplete="off"
+                                value="Date"
+                                placeholder="Date"
+                            >
+                        </div>
+                        <div class="col-12 col-sm-5 d-flex pt-3 d-block d-sm-none">
+                            '. $status_select .'
+                        </div>
+                        <div class="col-12 pt-3 d-block d-sm-none">
+                            <button 
+                                class="btn btn-primary distributor_search w-sm-100 text-center"
+                                data-clinic-id="'. $clinic->getId() .'"
+                            >
+                                <i class="fa-solid fa-magnifying-glass me-3"></i>
+                                SEARCH
+                            </button>
+                        </div>
+                        <div class="col-12 pt-3 pb-3 d-block d-sm-none">
+                            <button 
+                                class="btn btn-secondary distributor_refresh w-sm-100 text-center"
+                                data-clinic-id="'. $clinic->getId() .'"
+                            >
+                                <i class="fa-solid fa-rotate me-3"></i>
+                                CANCEL
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>';
 
